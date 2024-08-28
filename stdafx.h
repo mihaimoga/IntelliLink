493559d2217d355601315653ba8335aae0e65512
Stefan-Mihai MOGA <stefan-mihai@moga.doctor>
2024-08-28 19:43:13 +0300

    Updated copyright information

--- stdafx.h
+++ stdafx.h
@@ -1,5 +1,6 @@
 /* Copyright (C) 2014-2024 Stefan-Mihai MOGA
 This file is part of IntelliLink application developed by Stefan-Mihai MOGA.
+IntelliLink is an alternative Windows version to Online Link Managers!
 
 IntelliLink is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the Open
